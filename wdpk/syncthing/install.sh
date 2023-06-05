42f868db33a15f6ba7414adceb29e14cd90c3eb6
Msprg <18015488+Msprg@users.noreply.github.com>
2023-06-05 21:34:04 +0200

    syncthing: install.sh shellcheck fixup

--- wdpk/syncthing/install.sh
+++ wdpk/syncthing/install.sh
@@ -4,17 +4,17 @@ log=/tmp/debug_apkg
 
 echo "APKG_DEBUG: $0 $@" >> $log
 
-INSTALL_DIR=$(readlink -f $1)
-. ${INSTALL_DIR}/env
+INSTALL_DIR=$(readlink -f "$1")
+. "${INSTALL_DIR}"/env
 
-NAS_PROG=$(readlink -f $2)
+NAS_PROG=$(readlink -f "$2")
 
 log=/tmp/debug_apkg
 
 APKG_PATH="${NAS_PROG}/${APKG_NAME}"
 
 echo "move package data" >> $log
-mv ${INSTALL_DIR} ${NAS_PROG} 2>&1 >> $log
+mv "${INSTALL_DIR}" "${NAS_PROG}" 2>&1 >> $log
 
 # setup secure downloads
 if [ ! -e /etc/ssl/cert.pem ]; then
@@ -35,16 +35,16 @@ PACKAGE="${MAINDIR}.tar.gz"
 URL="https://github.com/syncthing/syncthing/releases/download/${VERSION}/${PACKAGE}"
 echo "Pulling ${URL}" >> $log
 
-curl -L -s ${URL} | tar zx -C ${APKG_PATH} 2>&1 >> $log
+curl -L -s ${URL} | tar zx -C "${APKG_PATH}" 2>&1 >> $log
 
 [[ ! $? -eq 0 ]] && exit 2
 
 # strip the version from the app dir
-mv ${APKG_PATH}/${MAINDIR} ${APKG_PATH}/${APKG_NAME}
+mv "${APKG_PATH}"/${MAINDIR} "${APKG_PATH}"/${APKG_NAME}
 
 # create syncthing home dir
 mkdir -p ${ST_HOME}
 # finds first user in the admin group
 ADMIN_USER=$(cat /etc/group | grep administrators | head -n 1 | awk -F: '{ print $4}')
-chown ${ADMIN_USER} ${ST_HOME}
+chown "${ADMIN_USER}" ${ST_HOME}
 chmod u+w ${ST_HOME}
